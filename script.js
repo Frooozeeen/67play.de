e49e7d7295dfa9acc43c8290a6dabb5a2050fd95
Jan <janhink2011@gmail.com>
2026-02-28 17:23:47 +0100

    good

---
+++ script.js
@@ -0,0 +1,13 @@
+const box1 = document.getElementById(box1);
+const box2 = document.getElementById(box2);
+const box3 = document.getElementById(box3);
+
+const img1 = document.getElementById(img1);
+const img2 = document.getElementById(img2);
+const img3 = document.getElementById(img3);
+
+window.alert("Alert")
+
+box1.addEventListener("click", () => {
+  window.location.href = "https://immich.67play.de";
+});
